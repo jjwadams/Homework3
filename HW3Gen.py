c04be6c1831a7a1299361768cfe6b56bee360dcc
Jesse Adams <jadams@math.arizona.edu>
2017-10-22 22:42:39 -0700

    updated generator function

--- HW3Gen.py
+++ HW3Gen.py
@@ -1,6 +1,9 @@
 import numpy as np
 import Homework3
 
-for a in np.arange( 0, 1, 0.01 ):
-	for r in np.arange( 0, 3, 0.1 ):
-		Homework3.main( a, r )
+def main( rga = np.linspace( 0, 1, 11 ), rgr = np.linspace( 0, 3, 4 ), 
+          strSaveDir = "./outFiles", strLoadFile = "HW3Data.dat" ):
+
+    for a in rga:
+        for r in rgr:
+            Homework3.main( a, r, strSaveDir, strLoadFile )
--- Homework3.py
+++ Homework3.py
@@ -59,14 +59,14 @@ def main( a = 0, r = 1, strSaveDir = "./outFiles", strLoadFile = "HW3Data.dat" )
     xOutSqrtMean = np.mean( xOutSqrt, axis = 1 )
     RMSESqrt = np.sqrt( np.mean( ( xTrue - xOutSqrtMean[ :, 1: ] ) ** 2, 0 ) )
 
-    strSaveFile = "{}/a{}r{}.dat"%( strSaveDir, a, r )
+    strSaveFile = "%s/a%sr%s.dat"%( strSaveDir, a, r )
     dictSave = { "spreadPO": spreadPO, "RMSEPO": RMSEPO, "spreadSqrt": spreadSqrt, 
                  "RMSESqrt": RMSESqrt }
 
     with open( strSaveFile, 'wb' ) as fiSave:
         pickle.dump( dictSave, fiSave )
 
-    strTextFile = "{}/test.txt"%( strSaveDir )
+    strTextFile = "%s/test.txt"%( strSaveDir )
     with open( strTextFile, 'a' ) as fiText:
         fiText.write( "Finished a = %f, r = %f\n"%( a, r ) ) 
 
